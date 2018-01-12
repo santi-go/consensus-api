adc63f7f22cd6f0cc07abef9e519f17078f63b21
Vollcode <daniel.ortiz.olivares@gmail.com>
2018-01-12 11:35:40 +0100

    change naming of send-mail endpoint to create-proposal

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     response.headers['Access-Control-Allow-Origin'] = '*'
   end
 
-  post '/send-mail' do
+  post '/create-proposal' do
     params = JSON.parse(request.body.read)
     consensus_email = 'consensus@devscola.org'
     circle = params['circle']
