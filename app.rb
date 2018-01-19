d50da81601d12242f5af058f215c1e9b8b2b41a1
Raül Martínez i Peris <raul@cuchame.es>
2018-01-19 16:44:11 +0100

    Add options to response headers

--- app.rb
+++ app.rb
@@ -7,6 +7,14 @@ require_relative './system/notify_involved'
 
 class App < Sinatra::Base
 
+  configure do
+    enable :cross_origin
+  end
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
   post '/create-proposal' do
     params = JSON.parse(request.body.read)
     involved = params['circle']
@@ -14,5 +22,12 @@ class App < Sinatra::Base
     proposal = params['proposal']
 
     Notify_involved.do(involved, proposal, proposer)
+  end
+
+  options "*" do
+    response.headers["Allow"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
   end
 end
