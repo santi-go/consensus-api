766d9f4fa68f192c23ce6abf289cd91a04ab5ce0
Raül Martínez i Peris <raul@cuchame.es>
2018-01-19 09:07:14 +0100

    Delete unnecessary code

--- app.rb
+++ app.rb
@@ -7,14 +7,14 @@ require_relative './system/notify_involved'
 
 class App < Sinatra::Base
 
-  set :bind, '0.0.0.0'
-  configure do
-    enable :cross_origin
-  end
-
-  before do
-     response.headers['Access-Control-Allow-Origin'] = '*'
-  end
+  # set :bind, '0.0.0.0'
+  # configure do
+  #   enable :cross_origin
+  # end
+  #
+  # before do
+  #    response.headers['Access-Control-Allow-Origin'] = '*'
+  # end
 
   post '/create-proposal' do
     params = JSON.parse(request.body.read)
