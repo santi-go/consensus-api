afa415b666846190cf8492c9e4808e784207a8b1
helenmg <elenamg31@gmail.com>
2018-02-19 12:46:10 +0100

    Add env to docker-compose.yml to privatize password

--- initializers/configure_mail_gem.rb
+++ initializers/configure_mail_gem.rb
@@ -5,7 +5,7 @@ Mail.defaults do
     address: 'smtp.sendgrid.net',
     port: 25,
     user_name: 'apikey',
-    password: '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',
+    password: ENV['KEY'],
     return_response: true
   }
 end
